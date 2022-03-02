4b44e04df433c22c43f783e5e2b9034964b0f5f3
Yop789 <2000djpz@gmail.com>
2022-03-02 14:27:52 -0600

    commit init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './componets/login/login.component';
+import { PrincipalComponent } from './componets/principal/principal.component';
+import { NavegadorComponent } from './componets/navegador/navegador.component';
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'home', component: PrincipalComponent },
+  { path: 'navejador', component: NavegadorComponent },
+  { path: 'login', component: LoginComponent },
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componets/login/login.component';
+import { PrincipalComponent } from './componets/principal/principal.component';
+import { NavegadorComponent } from './componets/navegador/navegador.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    LoginComponent,
+    AppComponent,
+    PrincipalComponent,
+    NavegadorComponent
+    
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
+   
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
